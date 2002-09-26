85649b8cf97e3ee1a80fed3df42bb0425bdb3bdf
Peter Palfrader <weasel@debian.org>
2002-09-26 08:21:07 +0000

    We now handle CRLF end of lines in pubring.mix

--- Src/keymgt.c
+++ Src/keymgt.c
@@ -6,7 +6,7 @@
    details.
 
    Key management
-   $Id: keymgt.c,v 1.14 2002/09/18 23:26:16 rabbi Exp $ */
+   $Id: keymgt.c,v 1.15 2002/09/26 08:21:07 weaselp Exp $ */
 
 
 #include "mix3.h"
@@ -147,7 +147,6 @@ static int getv2pubkey(byte keyid[], BUFFER *key)
   iv = buf_new();
   temp = buf_new();
   id_encode(keyid, idstr);
-  strcat(idstr, "\n");
   if ((keyring = mix_openfile(PUBRING, "r")) == NULL) {
     errlog(ERRORMSG, "Can't open %s!\n", PUBRING);
     err = -1;
@@ -159,6 +158,10 @@ static int getv2pubkey(byte keyid[], BUFFER *key)
     if (strleft(line, begin_key)) {
       if (fgets(line, sizeof(line), keyring) == NULL)
 	break;
+      if ((strlen(line) > 0) && (line[strlen(line)-1] == '\n'))
+	line[strlen(line)-1] = '\0';
+      if ((strlen(line) > 0) && (line[strlen(line)-1] == '\r'))
+	line[strlen(line)-1] = '\0';
       if (!streq(line, idstr))
 	continue;
       fgets(line, sizeof(line), keyring);	/* ignore length */
@@ -167,7 +170,7 @@ static int getv2pubkey(byte keyid[], BUFFER *key)
 	  goto done;
 	if (strleft(line, end_key))
 	  goto done;
-	buf_append(key, line, strlen(line) - 1);
+	buf_append(key, line, strlen(line));
       }
       break;
     }
