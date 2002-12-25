e6d07fd8be420f51e3a94cba7a8b84181a76b830
Len Sassaman <rabbi>
2002-12-25 08:31:39 +0000

    Preparing for 2.9.0 release.

--- Mix/Src/version.h
+++ Mix/Src/version.h
@@ -6,7 +6,7 @@
    details.
 
    Version definition file.
-   $Id: version.h,v 1.7.2.10 2002/12/17 21:31:32 rabbi Exp $ */
+   $Id: version.h,v 1.7.2.11 2002/12/25 08:31:39 rabbi Exp $ */
 
 
-#define VERSION "2.9.0rc1"
+#define VERSION "2.9.0"
