db68392c85a656bdd632ff4f91c49eefb9c93559
Len Sassaman <rabbi>
2003-12-21 03:30:09 +0000

    Removed AC experiment, restored the beast that is Install.

--- autogen.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-set -e
-
-aclocal-1.7 || aclocal-1.6 || aclocal
-autoheader
-automake-1.7 --add-missing --copy || automake-1.6 --add-missing --copy || automake --add-missing --copy
-autoconf
