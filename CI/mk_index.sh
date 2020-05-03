c8e97fd9d584f1375aa6498d64430d95b7cc8b7e
Benedikt Spranger <bene@eurovibes.org>
2020-05-03 17:43:37 +0200

    travis: Build and deploy documentation.
    The start page of the documentation was broken. Revisit the page
    generation.
    
    Signed-off-by: Benedikt Spranger <bene@eurovibes.org>

--- CI/mk_index.sh
+++ CI/mk_index.sh
@@ -3,6 +3,6 @@
 CLANG_RESULTS="clang/$(ls -1 report/clang)/index.html"
 GCC_RESULTS="gcc/$(ls -1 report/gcc)/index.html"
 
-sed -e "s!@STATIC_CLANG@!$(CLANG_RESULTS)!" \
-    -e "s!@STATIC_GCC@!$(GCC_RESULTS)!" \
+sed -e "s!@STATIC_CLANG@!${CLANG_RESULTS}!" \
+    -e "s!@STATIC_GCC@!${GCC_RESULTS}!" \
     CI/index.template > report/index.html
